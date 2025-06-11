4fede404b44390ec441f158ff00ac650fccc0b53
BobbyB-SLC <bb@skylenscarolina.com>
2025-06-10 22:24:00 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 const PDFDocument = require('pdfkit');
 const fs = require('fs');
 const path = require('path');
+const { getBriefingData } = require('./server/scraper');
+
 
 const app = express();
 const PORT = 3000;
